2fb1e2a95d5c58f42f0135cb496a69aa22dd611a
KanarXD <piciolo@outlook.com>
2022-01-18 13:22:22 +0100

    add javafx base

---
+++ gui/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module put.edu.gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires eu.hansolo.tilesfx;
+
+    opens put.edu.gui to javafx.fxml;
+    exports put.edu.gui;
+}
---
+++ gui/src/main/java/put/edu/gui/serverapi/ServerApi.java
@@ -0,0 +1,4 @@
+package put.edu.gui.serverapi;
+
+public class ServerApi {
+}
