1b0d9fc68249b05da47654b24f93d5d00023f33d
picio <piciolo@outlook.com>
2022-01-18 18:07:39 +0100

    add buttons resolving

--- gui/src/main/java/put/edu/gui/controllers/PlayerSceneController.java
+++ gui/src/main/java/put/edu/gui/controllers/PlayerSceneController.java
@@ -3,6 +3,7 @@ package put.edu.gui.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import put.edu.gui.KahootApplication;
@@ -10,13 +11,24 @@ import put.edu.gui.KahootApplication;
 import java.io.IOException;
 
 public class PlayerSceneController {
-
     @FXML
     public TextField gameNumberTextField;
 
     @FXML
     public void joinGame() {
 
+
+    }
+
+    public void sendAnswer(ActionEvent actionEvent) {
+        if (actionEvent.getSource() instanceof Button button) {
+            switch (button.getId()) {
+                case "A" -> System.out.println("A");
+                case "B" -> System.out.println("B");
+                case "C" -> System.out.println("C");
+                case "D" -> System.out.println("D");
+            }
+        }
 
     }
 
