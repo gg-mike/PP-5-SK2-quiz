54d39cda7d822ba8e5031a99ee835f6aa6430f89
picio <piciolo@outlook.com>
2022-01-18 15:36:45 +0100

    add scenes

--- gui/src/main/java/put/edu/gui/KahootApplication.java
+++ gui/src/main/java/put/edu/gui/KahootApplication.java
@@ -1,23 +1,38 @@
 package put.edu.gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 
 public class KahootApplication extends Application {
     public static final int width = 800;
     public static final int height = 500;
+    private static Stage stage;
+    private static Scene scene;
+    private static Parent parent;
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(KahootApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
         stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void showScene(ActionEvent actionEvent, String sceneFileName) throws IOException {
+        parent = FXMLLoader.load(Objects.requireNonNull(KahootApplication.class.getResource(sceneFileName)));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(parent, KahootApplication.width, KahootApplication.height);
         stage.setScene(scene);
         stage.show();
     }
--- gui/src/main/java/put/edu/gui/controllers/MainSceneController.java
+++ gui/src/main/java/put/edu/gui/controllers/MainSceneController.java
@@ -12,27 +12,19 @@ import put.edu.gui.KahootApplication;
 import java.io.IOException;
 import java.util.Objects;
 
+
 public class MainSceneController {
-    private Stage stage;
-    private Scene scene;
-    private Parent parent;
+
 
     @FXML
     public void showPlayerScene(ActionEvent actionEvent) throws IOException {
-        showScene(actionEvent, "player-view.fxml");
+        KahootApplication.showScene(actionEvent, "player-view.fxml");
     }
 
     @FXML
     public void showHostScene(ActionEvent actionEvent) throws IOException {
-        showScene(actionEvent, "host-view.fxml");
+        KahootApplication.showScene(actionEvent, "host-view.fxml");
     }
 
-    private void showScene(ActionEvent actionEvent, String sceneFileName) throws IOException {
-        parent = FXMLLoader.load(Objects.requireNonNull(KahootApplication.class.getResource(sceneFileName)));
-        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene(parent, KahootApplication.width, KahootApplication.height);
-        stage.setScene(scene);
-        stage.show();
-    }
 
 }
