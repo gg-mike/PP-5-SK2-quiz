34230b8b080ef3e2734ee05b52261d4cdd95dd54
KanarXD <piciolo@outlook.com>
2022-01-21 19:58:43 +0100

    add joinGame

--- gui/src/main/java/put/edu/gui/controllers/PlayerSceneController.java
+++ gui/src/main/java/put/edu/gui/controllers/PlayerSceneController.java
@@ -6,8 +6,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import put.edu.gui.KahootApp;
+import put.edu.gui.game.messages.Message;
 import put.edu.gui.game.messages.requests.AnswerMessage;
 import put.edu.gui.game.messages.requests.JoinGameMessage;
+import put.edu.gui.game.messages.responses.AcceptMessage;
+import put.edu.gui.game.messages.responses.DeclineMessage;
 
 import java.io.IOException;
 
@@ -28,9 +31,17 @@ public class PlayerSceneController {
             String username = usernameTextField.getText();
             int gameCode = Integer.parseInt(gameCodeTextField.getText());
             KahootApp.get().sendMessage(new JoinGameMessage(gameCode, username));
-
-            optionsGridPane.setVisible(true);
-            joinGameGridPane.setVisible(false);
+            if (KahootApp.get().getMessageObservable().isPresent()) {
+                Message responseMessage = KahootApp.get()
+                        .getMessageObservable()
+                        .get()
+                        .filter(message -> message instanceof AcceptMessage || message instanceof DeclineMessage)
+                        .blockingFirst();
+                if (responseMessage instanceof AcceptMessage) {
+                    optionsGridPane.setVisible(true);
+                    joinGameGridPane.setVisible(false);
+                }
+            }
         } catch (Exception e) {
             System.out.println("bad join game data");
         }
