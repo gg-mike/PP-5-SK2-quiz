c2da8f51ee7f0e89d6d9878e719bfcd0afab4228
KanarXD <piciolo@outlook.com>
2022-01-22 16:23:49 +0100

    add missing messages

--- gui/src/main/java/put/edu/gui/game/messages/MessageTypePair.java
+++ gui/src/main/java/put/edu/gui/game/messages/MessageTypePair.java
@@ -3,7 +3,11 @@ package put.edu.gui.game.messages;
 
 public enum MessageTypePair {
     AllAnsweredMessage(MessageType.ALL_ANSWERED.getValue()),
-    CreateGameMessage(MessageType.CREATE_GAME.getValue());
+    CreateGameMessage(MessageType.CREATE_GAME.getValue()),
+    CurrentResultsMessage(MessageType.CURRENT_RESULTS.getValue()),
+    GameEndedMessage(MessageType.GAME_ENDED.getValue()),
+    RoundEndedMessage(MessageType.ROUND_ENDED.getValue()),
+    RoundTimeoutMessage(MessageType.ROUND_TIMEOUT.getValue());
 
     private final int messageType;
 
